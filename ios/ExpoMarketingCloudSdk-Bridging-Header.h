fea6410b64c3e49bf8500f45e2fbf5e3db4d8870
juan-vilas <108366198+juan-vilas@users.noreply.github.com>
2025-03-31 12:56:31 +0200

    url handler ios

--- ios/ExpoMarketingCloudSdk-Bridging-Header.h
+++ ios/ExpoMarketingCloudSdk-Bridging-Header.h
@@ -4,3 +4,33 @@
 
 #import <EXNotifications/EXNotificationCenterDelegate.h>
 #import <MarketingCloudSDK/MarketingCloudSDK.h>
+
+// Implement the SFMCSdkURLHandlingDelegate delegate
+@interface AppDelegate : RCTAppDelegate<UNUserNotificationCenterDelegate, SFMCSdkURLHandlingDelegate>
+
+// AppDelegate.mm ----
+
+// This method is called after successfully initializing the SFMCSdk
+- (void)pushSetup {
+  dispatch_async(dispatch_get_main_queue(), ^{
+    // Here we set the URL Handling delegate to present URLs from CloudPages, OpenDirect, and In-App Messages
+    [[SFMCSdk mp] setURLHandlingDelegate:self];
+
+    // Set UNUserNotificationCenter delegate, register for remote notifications, etc...
+  });
+}
+
+// ...
+
+// Implement the required delegate method to handle URLs
+- (void)sfmc_handleURL:(NSURL * _Nonnull)url type:(NSString * _Nonnull)type {
+    if ([[UIApplication sharedApplication] canOpenURL:url]) {
+        [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:^(BOOL success) {
+            if (success) {
+                NSLog(@"url %@ opened successfully", url);
+            } else {
+                NSLog(@"url %@ could not be opened", url);
+            }
+        }];
+    }
+}
