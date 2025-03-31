a8c59e27fb5b62f68a9edb74e684da817d70dfd0
juan-vilas <108366198+juan-vilas@users.noreply.github.com>
2025-03-31 16:33:45 +0200

    fix

--- ios/ExpoMarketingCloudSdk-Bridging-Header.h
+++ ios/ExpoMarketingCloudSdk-Bridging-Header.h
@@ -1,27 +1,26 @@
 //
 // Use this file to import your target's public headers that you would like to expose to Swift.
 //
-
 #import <EXNotifications/EXNotificationCenterDelegate.h>
 #import <MarketingCloudSDK/MarketingCloudSDK.h>
+#import "RCTAppDelegate.h"
 
 // Implement the SFMCSdkURLHandlingDelegate delegate
-@interface AppDelegate : RCTAppDelegate<UNUserNotificationCenterDelegate, SFMCSdkURLHandlingDelegate>
+@interface AppDelegate : RCTAppDelegate <UNUserNotificationCenterDelegate, SFMCSdkURLHandlingDelegate>
+@end // Added semicolon here to close the @interface block
 
-// AppDelegate.mm ----
+@implementation AppDelegate
 
 // This method is called after successfully initializing the SFMCSdk
 - (void)pushSetup {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    // Here we set the URL Handling delegate to present URLs from CloudPages, OpenDirect, and In-App Messages
-    [[SFMCSdk mp] setURLHandlingDelegate:self];
-
-    // Set UNUserNotificationCenter delegate, register for remote notifications, etc...
-  });
+    dispatch_async(dispatch_get_main_queue(), ^{
+        // Here we set the URL Handling delegate to present URLs from CloudPages, OpenDirect, and In-App Messages
+        [[SFMCSdk mp] setURLHandlingDelegate:self];
+        // Set UNUserNotificationCenter delegate, register for remote notifications, etc...
+    });
 }
 
 // ...
-
 // Implement the required delegate method to handle URLs
 - (void)sfmc_handleURL:(NSURL * _Nonnull)url type:(NSString * _Nonnull)type {
     if ([[UIApplication sharedApplication] canOpenURL:url]) {
@@ -34,3 +33,5 @@
         }];
     }
 }
+
+@end
